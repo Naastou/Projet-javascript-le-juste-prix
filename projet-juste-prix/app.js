94e24da5ffb733d5c991b76419f3c95d881a6cbd
Na Astou <naastou1@gmail.com>
2023-05-11 22:01:23 +0200

    projet javascript le juste prix

---
+++ projet-juste-prix/app.js
@@ -0,0 +1,25 @@
+// 1.Initialiser un nombre aléatoire entre 1 et 500
+const getRandonNumber = Math.floor(Math.random() * 500) + 1;
+
+// 2. Demander à l'utilisateur de saisir un un chiffre max 10 fois
+let userImput;
+let i = 0;
+for (i = 1; i <= 10; i++) {
+  userImput = prompt("donnez un chiffre:");
+
+  // 3. Vérifier si le nombre est le juste prix et lui afficher un adéquat
+  if (userImput === getRandonNumber) {
+    alert("Bravo");
+    break;
+  }
+  if (userImput > getRandonNumber) {
+    alert("trop grand");
+  }
+  if (userImput < getRandonNumber) {
+    alert("trop petit");
+  } else {
+    alert("ce n'est pas un chiffre");
+    i -= 1;
+  }
+}
+alert("c'est fini. le chiffre cherche est : " + getRandonNumber);
